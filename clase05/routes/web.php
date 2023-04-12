186e251261ccfac532613ec0dafdfa1e0553918c
Edwin <ing.eliasincio123@gmail.com>
2023-04-11 23:34:37 -0500

    proyecto laravel 10

---
+++ clase05/routes/web.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+// PROTOCOLO HTTP
+// METODOS HTTP
+
+// MUESTRO PAGINAS
+// LISTO RESULTADOS
+//  metodo GET
+
+// REGISTRO NUEVOS DATOS
+// metodo POST
+
+// ACTUALIZO DATOS
+// metodo PUT/PATCH
+
+// ELIMINO DATOS
+// metodo DELETE
