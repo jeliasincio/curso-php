0f7a89d0635bcf033aa2fc412feebb7c59f9dae0
Edwin <ing.eliasincio123@gmail.com>
2023-04-14 21:26:38 -0500

    creacion de la tabla migration en la base de datos

--- clase05/database/migrations/2023_04_14_180113_crear_tabla_persona.php
+++ clase05/database/migrations/2023_04_14_180113_crear_tabla_persona.php
@@ -13,7 +13,18 @@ return new class extends Migration
     {
         Schema::create('persona', function (Blueprint $table) {
             $table->id();
+            $table->string('apellido_paterno',50)->comment('columna para el apellido paterno');
+            $table->string('apellido_materno',50);
+            $table->string('nombre',60);
+            $table->date('fecha_nacimineto');
+            $table->decimal('peso',15,7);
+            $table->char('dni',8);
+            $table->boolean('activo')->default(true);
+            $table->enum('sexo',['Masculino','Femenino']); // masculino o femenino
+            $table->enum('estado_civil',['Soltero','Casado','Viudo','Divorciado']);
             $table->timestamps();
+            $table->softDeletes(); //eliminación suave
+            //delete_at => timestamp => guardar la fecha y hora  de la eliminación del registro
         });
     }
 
